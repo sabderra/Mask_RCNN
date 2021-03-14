400f9db52baa9cf1638da575bae073e1f64947ce
sabderra <sadev1344@gmail.com>
2021-03-13 19:40:51 -0500

    Cast bool arrays to int for scikit-image 0.18.x

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -889,20 +889,18 @@ def resize(image, output_shape, order=1, mode='constant', cval=0, clip=True,
     """A wrapper for Scikit-Image resize().
 
     Scikit-Image generates warnings on every call to resize() if it doesn't
-    receive the right parameters. The right parameters depend on the version
+    receive the right parameters or types. The right parameters depend on the version
     of skimage. This solves the problem by using different parameters per
     version. And it provides a central place to control resizing defaults.
     """
-    if LooseVersion(skimage.__version__) >= LooseVersion("0.14"):
-        # New in 0.14: anti_aliasing. Default it to False for backward
-        # compatibility with skimage 0.13.
-        return skimage.transform.resize(
-            image, output_shape,
-            order=order, mode=mode, cval=cval, clip=clip,
-            preserve_range=preserve_range, anti_aliasing=anti_aliasing,
-            anti_aliasing_sigma=anti_aliasing_sigma)
-    else:
-        return skimage.transform.resize(
-            image, output_shape,
-            order=order, mode=mode, cval=cval, clip=clip,
-            preserve_range=preserve_range)
+
+    # Post 0.16, Interpolation is not defined with bool data type. Convert to
+    # int for skimage.transform.resize
+    if image.dtype == bool:
+        image = image.astype(int)
+
+    return skimage.transform.resize(
+        image, output_shape,
+        order=order, mode=mode, cval=cval, clip=clip,
+        preserve_range=preserve_range, anti_aliasing=anti_aliasing,
+        anti_aliasing_sigma=anti_aliasing_sigma)
